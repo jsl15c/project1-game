26519ee16868e3444e513e00fdef350610615901
Jarrod Luca <jarrodluca@gmail.com>
2017-06-14 16:11:39 -0400

    improve move function to add colliding tiles

--- js/game.js
+++ js/game.js
@@ -49,23 +49,22 @@ function makeTiles() {        // makeTiles() creates initial and proceeding tile
 
       gridArr[randomRow][randomCol] = 2;
     }
-    round = null;
   }
   else {
     var newTileRow, newTileCol;
+    round = null;
     newTileRow = Math.floor(Math.random() * 4);
     newTileCol = Math.floor(Math.random() * 4);
       for (var m = 0; m < gridArr.length; m++) {
         for (var n = 0; n < gridArr[m].length; n++) {
-          while (gridArr[newTileRow][newTileCol] === gridArr[m][n]) {
+          while ((newTileRow === m) && (newTileCol === n)) {
             newTileRow = Math.floor(Math.random() * 4);
             newTileCol = Math.floor(Math.random() * 4);
           }
         }
       }
-      // randomVal = initialTileVal[Math.floor(Math.random() * initialTileVal.length)];
       gridArr[newTileRow][newTileCol] = 2;
-  }
+    }
   fillTile();
 }
 
@@ -152,10 +151,26 @@ function moveRight() {
   for (var i = 0; i < gridArr.length; i++) {
     for (var j = 0; j < gridArr[i].length; j++) {
       if (gridArr[i][j] !== null && j !== 3) {
-        gridArr[i][3] = gridArr[i][j];  // transfers old value to new tile
-        gridArr[i][j] = null;           // removes value from old tile
-        $(`.cell-${i}-${j}`).removeClass('bg-2 bg-4 bg-8 bg-16 bg-32 bg-64 bg-128 bg-256 bg-512 bg-1024 bg-2084');
-        $(`.cell-${i}-3`).addClass('bg-2'); // function to determine which class to add
+        if (round === 1 && (gridArr[i][j] === gridArr[i][j + 2])) {
+          gridArr[i][3] = gridArr[i][j]*2;
+          gridArr[i][j] = null;
+          $(`.cell-${i}-${j}`).removeClass('bg-2 bg-4 bg-8 bg-16 bg-32 bg-64 bg-128 bg-256 bg-512 bg-1024 bg-2084');
+          $(`.cell-${i}-3`).addClass(`bg-${gridArr[i][3]}`); // function to determine which class to add
+        }
+        else if (gridArr[i][j] === gridArr[i][j + 1]) {
+          gridArr[i][3] = gridArr[i][j]*2;
+          gridArr[i][j] = null;
+          $(`.cell-${i}-${j}`).removeClass('bg-2 bg-4 bg-8 bg-16 bg-32 bg-64 bg-128 bg-256 bg-512 bg-1024 bg-2084');
+          $(`.cell-${i}-3`).addClass(`bg-${gridArr[i][3]}`); // function to determine which class to add
+        }
+
+        else {
+            gridArr[i][3] = gridArr[i][j];    // transfers old value to new tile
+            gridArr[i][j] = null;             // removes value from old tile
+            $(`.cell-${i}-${j}`).removeClass('bg-2 bg-4 bg-8 bg-16 bg-32 bg-64 bg-128 bg-256 bg-512 bg-1024 bg-2084');
+            $(`.cell-${i}-3`).addClass('bg-2'); // function to determine which class to add
+          }
+
       }
     }
   }
@@ -170,31 +185,46 @@ $(document).ready(function() {
     // up arrow key
     if (event.which === 38) {
       moveUp();
-      makeTiles();
+      // makeTiles();
       return;
       // makeTiles();
     }
     // right arrow key
     else if (event.which === 39) {
       moveRight();
-      makeTiles();
+      // makeTiles();
       return;
     }
     // down arrow key
     else if (event.which === 40) {
       moveDown();
-      makeTiles();
+      // makeTiles();
       return;
 
     }
     // left arrow key
     else if (event.which === 37) {
       moveLeft();
-      makeTiles();
+      // makeTiles();
       return;
     }
   });
 });
+
+// function addCol() {
+//   for (var i = 0; i < gridArr.length; i++) {
+//     for (var j = 0; j < gridArr.length; j++) {
+//       if (round === 1 && (gridArr[i][j]) === gridArr[i + 2][j]) {
+//         gridArr[i][j] = gridArr[i][j] + gridArr[i + 2][j];
+//         fillTile();
+//       }
+//       else if (gridArr[i][j] === gridArr[i + 1][j]) {
+//         gridArr[i][j] = gridArr[i][j] + gridArr[i + 1][j];
+//         fillTile();
+//       }
+//     }
+//   }
+// }
 
 // starts game, calls other functions
 function startGame() {
